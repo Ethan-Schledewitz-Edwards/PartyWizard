1d884e2384420c96eea42578f94754b797f98559
JoshG-OTU <joshua.geelen@ontariotechu.net>
2025-10-25 15:58:39 -0400

    Highlighted Script and CManager

--- Assets/Combat/CombatManager.cs
+++ Assets/Combat/CombatManager.cs
@@ -1,6 +1,55 @@
+using System;
 using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
-	
+    public static CombatManager Instance;
+
+    [SerializeField] private Player m_player;
+    [SerializeField] private Encounter[] m_encounters;
+
+    // System
+    private int encounterIndex;
+    public Encounter currentEncounter;
+
+    // Events
+    public Action<Encounter> OnEncounterBegin;
+    public Action<Encounter> OnEncounterEnd;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+    private void Start()
+    {
+        encounterIndex = 0;
+        BeginEncounter(encounterIndex);
+    }
+
+    private void BeginEncounter(int encounterID)
+    {
+        Encounter encouner = m_encounters[encounterID];
+
+        //m_player.transform.position = encouner.PlayerPos.position;
+
+        OnEncounterBegin?.Invoke(encouner);
+    }
+
+    private void FinishEncounter()
+    {
+        // Move player
+
+        OnEncounterBegin?.Invoke(currentEncounter);
+        currentEncounter = null;
+    }
+
+    public void AdvanceEncouner()
+    {
+        encounterIndex++;
+        BeginEncounter(encounterIndex);
+    }
 }
