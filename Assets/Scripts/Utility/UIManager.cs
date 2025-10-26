789415201f4aad527ce9178c9cd50656fd801b70
Ethan-Schledewitz-Edwards <144922288+Ethan-Schledewitz-Edwards@users.noreply.github.com>
2025-10-26 14:36:07 -0400

    Audio setup

--- Assets/Scripts/Utility/UIManager.cs
+++ Assets/Scripts/Utility/UIManager.cs
@@ -26,6 +26,17 @@ public class UIManager : MonoBehaviour
 	[SerializeField] private GameObject m_textPanel;
 	[SerializeField] private TextMeshProUGUI m_textBox;
 
+	[Header("GameStates")]
+	[SerializeField] private GameObject m_whiteLightPanel;
+	[SerializeField] private GameObject m_gameOverPanel;
+	[SerializeField] private GameObject m_playerWonPanel;
+
+	[Header("Audio")]
+	[SerializeField] private AudioSource m_audioSource;
+	[SerializeField] private AudioClip m_ringing;
+	[SerializeField] private AudioClip m_death;
+	[SerializeField] private AudioClip m_won;
+
 	[HideInInspector] public SO_Attack m_selectedAttack;
 	private InputSystem_Actions m_inputActions;
 
@@ -212,7 +223,10 @@ public class UIManager : MonoBehaviour
 
 	private void FireBullet()
 	{
-		Debug.Log("YOU DIED");
+		m_whiteLightPanel.SetActive(true);
+
+		// Play tinitus ringing sound
+		m_audioSource.PlayOneShot(m_ringing);
 	}
 
 	private void RechargeAdrenaline()
@@ -301,7 +315,20 @@ public class UIManager : MonoBehaviour
         DisplayPlayerOptions(true);
     }
     #endregion
-    public void UseAttack(SO_Attack attackData)
+
+	public void GameOver()
+	{
+		m_gameOverPanel.SetActive(true);
+		m_audioSource.PlayOneShot(m_death);
+	}
+
+	public void Won()
+	{
+		m_playerWonPanel.SetActive(true);
+		m_audioSource.PlayOneShot(m_won);
+	}
+
+	public void UseAttack(SO_Attack attackData)
     {
 		CombatManager combatManager = CombatManager.Instance;
 
